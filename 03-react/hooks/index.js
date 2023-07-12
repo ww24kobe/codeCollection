128ea95c5638ad6ebaa8cb3e91bdef2341c89b35
ww24kobe <1259481020@qq.com>
2023-07-12 11:22:27 +0800

    feat: useWatch-立即执行

--- 03-react/hooks/index.js
+++ 03-react/hooks/index.js
@@ -1,5 +1,6 @@
 import useTitle from './useTitle.js'
 import useMouse from './useMouse.js'
 import useUserData from './useUserData.js'
+import useWatch from './useWatch.js'
 
-export { useTitle, useMouse, useUserData }
+export { useTitle, useMouse, useUserData, useWatch }
